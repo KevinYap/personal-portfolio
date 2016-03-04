e30ba8d91af8a7f52114f19b176d93fd2110cfc0
Kevin Yap <kahchunyap@gmail.com>
2016-03-04 10:54:26 +0800

    2016 portfolio revamp - Gulpfile.js setup, changed to stylus.

---
+++ 2016/gulpfile.js
@@ -0,0 +1,86 @@
+var gulp = require('gulp'); // tells Node to look into the node_modules folder for a package named gulp
+var stylus = require('gulp-stylus');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
+
+gulp.task('hello', function(){
+	console.log('Hello K!');
+});
+
+gulp.task('stylus', function(){
+	return gulp.src('app/styl/app.styl')
+		.pipe(stylus()) // Using gulp-stylus
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({ // browser sync
+	     	stream: true
+	    }))
+});
+
+// Gulp watch task
+gulp.task('watch', ['browserSync', 'stylus'], function(){
+	// Reloads the browser whenever HTML or JS files change
+	// Other watchers (can be multiple watcher)
+	gulp.watch('app/styl/**/*.styl', ['stylus']);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+// Browser Sync task
+gulp.task('browserSync', function(){
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		},
+	})
+});
+
+// Useref task (concat files)
+gulp.task('useref', function(){
+	return gulp.src('app/*.html')
+		.pipe(useref())
+		// Minifies if it's a JavaScript file or CSS file
+	    .pipe(gulpIf('*.js', uglify()))
+	    .pipe(gulpIf('*.css', cssnano()))
+	    .pipe(gulp.dest('dist'))
+});
+
+// Optimizing images task
+gulp.task('images', function(){
+	return gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
+		.pipe(cache(imagemin({ // create cache for images
+			interlaced: true
+		})))
+		.pipe(gulp.dest('dist/img'))
+});
+
+// Clear cache
+gulp.task('cache:clear', function (callback) {
+	return cache.clearAll(callback)
+});
+
+// Copy fonts to dist folder
+gulp.task('fonts', function() {
+  	return gulp.src('app/fonts/**/*')
+	.pipe(gulp.dest('dist/fonts'))
+});
+
+// Cleaning in dist folder
+gulp.task('clean:dist', function() {
+  	return del.sync('dist');
+});
+
+// Development
+gulp.task('default', function(callback) {
+  	runSequence(['stylus','browserSync', 'watch'], callback)
+})
+
+// Build all the stuff into dist (Production)
+gulp.task('build', function(callback) {
+  	runSequence('clean:dist', ['stylus', 'useref', 'images', 'fonts'], callback)
+});
